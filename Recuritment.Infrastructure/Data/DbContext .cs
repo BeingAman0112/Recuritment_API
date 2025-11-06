4e1dca338118e66b5450eee6e9aa8ab0c4140fa2
Abhishek <abhishek.barman@eudemonic.co.in>
2025-11-06 13:06:18 +0530

    change from mssql to postgres connection

--- Recuritment.Infrastructure/Data/DbContext .cs
+++ Recuritment.Infrastructure/Data/DbContext .cs
@@ -1,26 +1,22 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using Microsoft.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Npgsql;
 
-namespace Recruitment.infrastructure.Data
+namespace Recruitment.Infrastructure.Data
 {
-    public class DbContext 
+    public class DbContext
     {
         private readonly IDbConnection _connection;
 
         public DbContext(IConfiguration configuration)
         {
-            // Reads connection string from appsettings.json
+            // Read connection string from appsettings.json or environment variable
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-            _connection = new SqlConnection(connectionString);
+
+            // Initialize PostgreSQL connection
+            _connection = new NpgsqlConnection(connectionString);
         }
 
         public IDbConnection Connection => _connection;
-
     }
 }
